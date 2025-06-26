e6cb6c3c57b8df1f50a451c68996493a0d5b2892
user <user@MacBook-Air-3.local>
2025-06-26 10:48:31 +0600

    pull

--- config/db.js
+++ config/db.js
@@ -1,20 +1,17 @@
 require('dotenv').config(); 
 const pgpInit = require('pg-promise');
 
-
-// ✅ Debug to confirm environment variables are working
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_PASSWORD:', typeof process.env.DB_PASSWORD); // should log: string
-
 const pgp = pgpInit();
 
 const db = pgp({
-    host: 'localhost',
-    port: process.env.DB_PORT,
+    host: process.env.DB_HOST, // ✅ make sure this is now correct
+    port: 5432,
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD, // ✅ make sure this is now correct
-    ssl: false,
+    ssl: {
+        rejectUnauthorized: false // ✅ make sure this is now correct
+    }
 });
 
 module.exports = db;
